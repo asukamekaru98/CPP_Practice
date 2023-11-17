d2cd18fb00f246b481637fd6898ecd6625c751cd
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-11-17 22:31:00 +0900

    ロベールのC++入門講座 02-02

---
+++ Practice_CPP/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   // std::cout << "Hello World!\n";
+
+	
+
+#if 1//テンプレート
+
+#endif
+
+#if 1//01-03 ほあ
+	cout << "9.0 / 2 = " << 9.0 / 2 << endl;
+#endif
+
+#if 0//01-03 2
+	cout << "9 / 2 = " << 9 / 2 << endl;
+	cout << "9 / 2 = " << 9 / 2 << endl;
+	cout << "2 + 2 * 4 = " << 2 + 2 * 4 << endl;
+	cout << "(1 - 5) / 2 = " << (1 - 5) / 2 << endl;
+#endif
+
+#if 0//01-03 1
+	cout << "2 + 2 = " << 2 + 2 << endl;
+	cout << "1 - 5 = " << 1 - 5 << endl;
+	cout << "2 * 9 = " << 2 * 9 << endl;
+	cout << "9 / 3 = " << 9 / 3 << endl;
+	cout << "9 / 2 = " << 9 / 2 << endl;
+#endif
+
+#if 0	//01-02
+	cout << 2 << '+' << 3 << '=' << 2 + 3 << endl;
+	cout << "π=" << 3.14159 << "..." << endl;
+#endif
+
+	
+
+
+}
