7dd5f1aef6721021b59a53d399df8f6bdc510086
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-11-19 22:20:44 +0900

    ロベールのC++入門講座 02-06 *メモ* ・cinは「character input」の意味 ・C++の標準規格では、main関数の戻り値はint型であることが要求されている。つまりint main()以外はだめ

---
+++ Practice_CPP/Input1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a;
+
+int main() {
+	cout << "‚È‚É‚©’l‚ð“ü‚ê‚Ä‚Ë > " << flush;
+	cin >> a;
+
+	cout << "‚»‚Ì”‚ð3‚ÅŠ„‚Á‚½‚ ‚Ü‚è‚Í" << a % 3 << "‚Å‚·" << endl;
+}
+
+
---
+++ Practice_CPP/func2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void Hello() {
+	cout << "やあ、こんにちは(ドナルド・マクドナルド)" << endl;
+}
+
+void FirstTime() {
+	cout << "私宇宙人なんだよ" << endl;
+	cout << "初めて言うけど" << endl;
+	cout << "初めて言っちゃうけど" << endl;
+}
+
+int main()
+{
+	Hello();
+	Hello();
+	Hello();
+	Hello();
+	FirstTime();
+}
