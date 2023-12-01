ebc70405407e086c0722d610bbe543e914953216
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-12-01 23:54:58 +0900

    ロベールのC++入門講座 03-10
    *メモ*
    オーバーロードはCではできない模様

---
+++ Practice_CPP/Math1.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1	//sprintŒx‚ğ–³‹‚³‚¹‚é
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, b;
+
+	cout << "’¼Šp‚ğ‹²‚Ş2•Ó‚Ì’·‚³‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B > " << flush;
+
+	cin >> a >> b;
+
+	cout << "Î•Ó‚Ì’·‚³‚Í" << sqrt(a * a + b * b) << "‚Å‚·B" << endl;
+}
---
+++ Practice_CPP/NumericalErrer1.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1	//sprintŒx‚ğ–³‹‚³‚¹‚é
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	for (double i = 0; i < 1; i += 0.1) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
