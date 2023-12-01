55e28030de029995dbd992b9f95567c900d41562
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-12-01 23:54:58 +0900

    ロベールのC++入門講座 03-09
    *メモ*
    floatとdouble
    基本的にはdoubleのほうが精度が良い。
    特別なこと（サイズや3Dでの使用など)がない限りは、doubleを使うべき、とのこと。

---
+++ Practice_CPP/NumericalError1b.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1	//sprintŒx‚ğ–³‹‚³‚¹‚é
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	for (double i = 0; i < 10; ++i) {
+		cout << (i / 10.0) << ' ';
+	}
+	cout << endl;
+}
