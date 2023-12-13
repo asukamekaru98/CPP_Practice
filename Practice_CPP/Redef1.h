487414082ed96075f1994fb1007f7f5b03620671
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-12-13 22:17:03 +0900

    ロベールのC++入門講座 05-11

---
+++ Practice_CPP/Redef1.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef REDEF1_H_
+#define REDEF1_H_
+
+const int N = 10;
+
+#endif
---
+++ Practice_CPP/redef1.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1	//sprintŒx‚ğ–³‹‚³‚¹‚é
+
+#include "Redef1.h"
+#include "Redef1.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << N << endl;
+}
