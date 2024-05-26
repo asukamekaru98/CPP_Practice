0b3acd259585a42771347065949ec4b9f39f297e
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-05-26 16:29:01 +0900

    11-01

---
+++ Practice_CPP/Integer.cpp
@@ -0,0 +1,14 @@
+#include "Integer.h"
+#include <iostream>
+
+using namespace std;
+
+static int m_value;
+
+Integer::Integer(int num) {
+	m_value = num;
+}
+
+void Integer::Show() {
+	cout << m_value << endl;
+}
---
+++ Practice_CPP/StaticMember1.cpp
@@ -0,0 +1,9 @@
+#include "Integer.h"
+#include <iostream>
+
+int main()
+{
+	Integer a(100), b(300);
+	a.Show();
+	b.Show();
+}
