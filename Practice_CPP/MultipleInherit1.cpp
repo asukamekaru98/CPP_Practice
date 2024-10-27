8da5d9ba02aeb4c4cd3215bae768bd7999a67d73
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-10-27 23:46:20 +0900

    13-01

---
+++ Practice_CPP/MultipleInherit1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class ABase {
+public:
+	virtual~ABase() {}
+	virtual void AHoge() = 0;
+};
+
+class BBase {
+public:
+	virtual~BBase() {}
+	virtual void BHoge() = 0;
+};
+
+class C :
+	public ABase,
+	public BBase
+{
+public:
+	virtual void AHoge() {
+		cout << "CBase::AHoge()" << endl;
+	}
+
+	virtual void BHoge() {
+		cout << "CBase::BHoge()" << endl;
+	}
+};
+
+int main() {
+	C c;
+
+	ABase& a = c;
+	a.AHoge();
+
+	BBase& b = c;
+	b.BHoge();
+}
